a24a8130cb9e7a2f1ac54a046db81c6f96fde667
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-16 12:59:09 -0500

    Migrated to storybook components

--- frontend/src/app/lessons/[slug]/page.tsx
+++ frontend/src/app/lessons/[slug]/page.tsx
@@ -1,13 +1,46 @@
 "use client";
 
-import LessonPage from "@/components/LessonPage";
+import XBlockExecutable from "@/components/XBlockExecutable";
+import XBlockText from "@/components/XBlockText";
 import { dummyLessons } from "@/lib/dummyLessons";
 import { useParams } from "next/navigation";
+
+import { submitCode } from "@/lib/submitCode";
 
 const page = () => {
   const params = useParams();
   const lesson = dummyLessons.find((lesson) => lesson.id === params.slug);
-  return <div>{lesson ? <LessonPage lesson={lesson} /> : <div>Lesson not found</div>}</div>;
+
+  return (
+    <div>
+      {lesson ? (
+        <div>
+          <p className="text-2xl font-bold text-center">{lesson.title}</p>
+          <div className="flex flex-col space-y-4 p-10">
+            {lesson.lessonBody.map((block, key) => {
+              if (block.type === "text") {
+                return <XBlockText key={key} text={block.content} />;
+                // return <LessonBlockText key={key} block={block} />;
+              } else if (block.type === "executable") {
+                // return <LessonBlockExecutable key={key} block={block} />;
+                return (
+                  <XBlockExecutable
+                    key={key}
+                    text={block.content}
+                    placeholder=""
+                    answer={block.answer}
+                    onSubmit={submitCode}
+                  />
+                );
+              }
+            })}
+          </div>
+        </div>
+      ) : (
+        <div>Lesson not found</div>
+      )}
+    </div>
+  );
 };
 
 export default page;
--- frontend/src/components/XBlockText.tsx
+++ frontend/src/components/XBlockText.tsx
@@ -6,13 +6,11 @@ interface XBlockTextProps {
 
 const XBlockText = ({ text }: XBlockTextProps) => {
   return (
-    <div className="text-xs font-mono bg-gray-800 text-white shadow-sm py-6 px-4">
-      {text.split("\n").map((line, index) => (
-        <>
-          <p key={index} className="pb-2">
-            {line}
-          </p>
-        </>
+    <div className="text-xs font-mono bg-gray-100 text-black shadow-sm py-6 px-4">
+      {text.split("\n").map((line, key) => (
+        <p key={key} className="pb-2">
+          {line}
+        </p>
       ))}
     </div>
   );
